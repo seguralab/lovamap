b2a7fbee4c8218d553bb5a455abe53c48b6ec917
Peter Cheng <pcheng17@gmail.com>
2022-08-31 12:48:33 -0700

    Add missing Excel output file

---
+++ src/num2ExcelCol.m
@@ -0,0 +1,11 @@
+% Convert numbers to capital letters that correspond to Excel columns
+
+function l = num2ExcelCol(n)
+    if n <= 26
+        l = char(n + 64);
+    else
+        first_letter = char(floor(n / 26) + 64);
+        second_letter = char(mod(n, 26) + 64);
+        l = [first_letter, second_letter];
+    end
+end
