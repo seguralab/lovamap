338004dea2c5ae9a059ad8841a1080ecb8ab0d88
lindsayriley <46536744+lindsayriley@users.noreply.github.com>
2024-05-15 06:01:02 -0400

    Updated with doors data

--- src/pathsCenter.m
+++ src/pathsCenter.m
@@ -8,7 +8,7 @@
 % voxels:             list of [x,y,z] coordinates of all gridpoints in domain
 % domain:             [xmin, xmax, ymin, ymax, zmin, zmax]
  
-function [center_peak, path_nodes, path_length, path_edges, path_tortuosity, path_necks] = ...
+function [center_peak, path_nodes, path_length, path_edges, path_tortuosity, path_necks, path_r1D_doors] = ...
                pathsCenter(peak_inds, pks_graph_length, ridges1D, ...
                edge_ind, voxels, domain)
  
@@ -152,13 +152,24 @@ function [center_peak, path_nodes, path_length, path_edges, path_tortuosity, pat
     path_tortuosity_vol = path_tortuosity_vol(path_tortuosity_vol ~= 0);
 
     % For each path, store the bottleneck widths (diameter) along 1D-ridges
-    path_necks  = cell(length(edge_pks), 1);
+    path_necks     = cell(length(edge_pks), 1);
+    path_doors     = cell(length(edge_pks), 1);
+    path_r1D_doors = cell(length(edge_pks), 1);
     for i = 1 : numel(path_edges)
         necks = zeros(numel(path_edges{i}), 1);
+        % make the following code more concise
+        door_bool = ~fastIntersect(path_edges{i}, data.ridges1D.connected, 'bool vec'); % finding 'doors' (i.e., the min on 1D ridges) that only exist between pores (not within pores)
+        these_edges = path_edges{i};
+        path_r1D_doors{i} = these_edges(door_bool);
+        doorss = zeros(numel(path_r1D_doors{i}), 1);
         for j = 1 : numel(path_edges{i})
-            necks(j) = ridges1D.doors{path_edges{i}(j)}.radius * 2;
+            necks(j) = data.ridges1D.doors{path_edges{i}(j)}.radius * 2;
+        end
+        for k = 1 : numel(path_r1D_doors{i})
+            doorss(k) = data.ridges1D.doors{path_r1D_doors{i}(k)}.radius * 2;
         end
         path_necks{i} = necks;
+        path_doors{i} = doorss;
     end
 
     % save
