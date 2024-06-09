e7c0a85a6680feb4256b8a99519b20d4c24140cc
lindsayriley <46536744+lindsayriley@users.noreply.github.com>
2024-06-09 19:37:01 -0400

    Fixed bug in num2ExcelCol.m

--- src/num2ExcelCol.m
+++ src/num2ExcelCol.m
@@ -4,8 +4,8 @@ function l = num2ExcelCol(n)
     if n <= 26
         l = char(n + 64);
     else
-        first_letter = char(floor(n / 26) + 64);
-        second_letter = char(mod(n, 26) + 64);
+        first_letter = char(floor((n - 1) / 26) + 64);
+        second_letter = char(mod((n - 1), 26) + 65);
         l = [first_letter, second_letter];
     end
 end
